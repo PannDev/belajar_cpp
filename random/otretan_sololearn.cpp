fe90997fc2df949246059cb94f15691a4b860d9a
PannDev <dryhezelnut@gmail.com>
2023-03-18 12:29:06 +0700

    added otretan sololearn

---
+++ random/otretan_sololearn.cpp
@@ -0,0 +1,117 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+//#define PannDev ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+//void epan(int a, int b) {
+//	if (a > b) {
+//		cout << "A lebih besar dari B";
+//	} else if (a < b) {
+//		cout << "A lebih kecil dari B";
+//	} else {
+//		cout << " A = B";
+//	}
+//}
+
+//int main() {
+////	int a,b;
+////	cin >> a >> b;
+////	epan(a,b);
+//	 
+//	}
+
+//void add(string a, string b) {
+//	cout << a << " " << b;
+//}
+
+int main() {
+	
+int bintang = 0;
+int i; cin >> i;
+
+while (i < bintang) {
+	cout << bintang << endl;
+	bintang++;
+}
+
+// ketika 0 lebih kecil dari 3
+// maka keluarkan bintang
+// i = 4
+
+
+	
+	
+	
+	
+	
+	 
+//	int postfix = a++;
+//	
+//	cout << prefix << " " << postfix << endl;
+//	
+//	int respre = 1 + ++a;
+//	int respost = 1 + a++;
+//	cout << respre << " " << respost << endl;
+	return 0;
+//string a,b;
+//getline(cin, a);
+//getline(cin, b);
+//add(a,b);
+
+}
+
+	
+	
+	
+	
+	
+	//PannDev
+
+
+//int arr[] = {1,2,3,4,5};
+//int *ptr = arr;
+//cout << arr[2] << " " << *(ptr+2) << endl;
+
+//int arr[] = {2,4,6,8};
+//
+//int *p = arr;
+//for(int i =0;i<4;i++) {
+//	cout << *(p+i) << endl;
+//}
+
+// pointers
+//int a =4;
+//int *p = &a;
+//cout << *p << " " << a << " " << &a << endl;
+//a +=2;
+//*p += 3;
+//a-= 1;
+//cout << a;
+
+// multi dimensi array
+/* int x[3][2] = { {2,4}, {6,8}, {1,2} }; */
+//out< x[2][1];	dk
+//char huruf[] = {'a', 'b'};
+//int angka[] = {1,2, 3};
+//
+//cout << huruf;
+//
+//int keluar; cin >> keluar;
+//for(auto x : angka) {
+//	keluar += x;
+//}
+//cout << keluar << endl;
+//
+//
+//double prices [] = {3.3, 4.2, 5.1, 8.1, 4.4, 7.3}; deklarasi price
+
+
+
+
+//double res = 0;
+//for (auto x : prices) {
+//	res += x;
+//}
+// 	cout << res;
+ 	
