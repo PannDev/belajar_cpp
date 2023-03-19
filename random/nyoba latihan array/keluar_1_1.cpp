84b7c03e9a6ba8e912989971b539ffdc6ca72986
PannDev <dryhezelnut@gmail.com>
2023-03-19 11:31:57 +0700

    tes program

--- random/nyoba latihan array/keluar_1_1.cpp
+++ random/nyoba latihan array/keluar_1_1.cpp
@@ -1,1 +1,16 @@
 // Latihan keluarin array satu satu
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+#define PannDev ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+int main() {
+    PannDev
+    
+    int arr[] = {1,2,3,4,5};
+    cout << arr[0];
+
+    return 0;
+}
