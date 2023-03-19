94125b97d05d6b5786fe97c4969f5674ca70e8c5
Evan <86787046+PannDev@users.noreply.github.com>
2023-03-19 14:34:24 +0700

    added b cpp

---
+++ tlx/Pemograman Dasar/(09)_Array/b.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+#define PannDev ios_base::sync_with_stdio(0); cin(0); cout(0);
+
+int main() {
+    PannDev
+
+
+    return 0;
