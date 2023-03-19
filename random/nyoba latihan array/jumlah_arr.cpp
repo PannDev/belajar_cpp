344f7eaf6e6bc40e54c7cf99e137a1bcc1b6b2c9
Evan <86787046+PannDev@users.noreply.github.com>
2023-03-19 13:23:13 +0700

    added jumlah arr

---
+++ random/nyoba latihan array/jumlah_arr.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+// #define PannDev ios_base::sync_with_stdio(0); cin(0); cout(0);
+
+int main() {
+    // PannDev
+    char arr[] = { '1', '2', '3', '4', '5' }; 
+    // note 
+    // arr char pakai ' '
+    // arr string pakai " "
+
+    for (int i = 0; i < 5; i++) {
+        cout << arr[i] << endl; // keluarin isi didalam array
+    }
+
+    return 0;
+}
