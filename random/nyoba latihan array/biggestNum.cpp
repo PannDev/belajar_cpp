c51c54339327f64ce74c8abfd8332d9ef53b56cf
PannDev <86787046+PannDev@users.noreply.github.com>
2023-03-23 22:59:55 +0700

    added biggestNum

--- random/nyoba latihan array/biggestNum.cpp
+++ random/nyoba latihan array/biggestNum.cpp
@@ -1,1 +1,23 @@
 // 4 3 7 2 ==> 7 tes
+#include <bits/stdc++.h>
+using namespace std;
+
+int main () {
+int n; cin >> n; 
+int arr[n];
+
+// n = banyak data
+for (int i = 0; i < n; i++) {
+    cin >> arr[i];
+}
+
+int max = arr[0];
+for (int i = 1; i < n; i++) {
+    if (arr[i] > max) {
+        max = arr[i];
+    }
+}
+cout << max << endl;
+
+    return 0;
+}
