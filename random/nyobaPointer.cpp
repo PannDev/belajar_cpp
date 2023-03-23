8f7456351510aa4853fa75457328802136a88368
PannDev <dryhezelnut@gmail.com>
2023-03-23 11:18:25 +0700

    added pointer tp gtau

---
+++ random/nyobaPointer.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+
+int main ( ) {
+    int a, b;
+    cin >> a >> b;
+    cout << a << b;
+    *a = a;
+    *b = b;
+    cout << a << b;
+
+
+    return 0;
+}
