4394ba6620702dc8b1380aecc43cd40610f721a4
PannDev <dryhezelnut@gmail.com>
2023-03-23 19:52:50 +0700

    lanjutin belajar reverse array

--- random/nyoba latihan array/reverseArray.cpp
+++ random/nyoba latihan array/reverseArray.cpp
@@ -1,1 +1,18 @@
-// 5 7 1 4 ==> 4 1 7 5
+// 5 7 1 4 ==> 4 1 7 5 nyoba reverse array
+#include <bits/stdc++.h>
+using namespace std;
+
+int main ( ) {
+    int n; cin >> n;
+    int arr[n];
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    for (int i = n; i >= 0; i--) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
