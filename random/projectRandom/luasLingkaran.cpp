8f27dd880b59ef45f15a5de03b7e85fe41b4d2dd
PannDev <86787046+PannDev@users.noreply.github.com>
2023-03-23 23:43:53 +0700

    added luasLingkaran

---
+++ random/projectRandom/luasLingkaran.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define PI 3.14
+
+double hitungLuas(int r) {
+    return PI * r * r;
+}
+
+void tampilLuas(int r) {
+    cout << "Luas Lingkaran dengan jari-jari " << r << " adalah " << hitungLuas(r) << endl;
+}
+
+int main( ) {
+    cout << "masukan jari jari = ";
+    int r; cin >> r;
+    tampilLuas(r);
+
+    return 0;
+}
