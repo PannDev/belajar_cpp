ffebec56319ac73f81b16d9ec7908bbd234de260
PannDev <dryhezelnut@gmail.com>
2023-04-03 22:44:05 +0700

    added namaHari

--- desktopBackup/namaHari_Angka.cpp
+++ desktopBackup/namaHari_Angka.cpp
@@ -4,25 +4,21 @@
 #define r0 return 0;
 using namespace std;
 
-// void solve() {
+//void solve() {
 //	int a; cin >> a;
 //	string day[] = { "Senin", "Selasa","Rabu","Kamis","Jumat","Sabtu","Minggu" };
 //	auto res = (a >= 1 & a <= 7) ? day[a-1] : "Harap Masukan 1 - 7";
 //	cout << res << endl;
-// }
+//}
 
-string solve()
-{
-    int a;
-    cin >> a;
-    string day[] = {"Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu", "Minggu"};
-    return (a >= 1 & a <= 7) ? day[a - 1] : "Harap Masukan 1 - 7";
+string solve() {
+	int a; cin >> a;
+	string day[] = { "Senin", "Selasa","Rabu","Kamis","Jumat","Sabtu","Minggu" };
+	return (a >= 1 & a <= 7) ? day[a-1] : "Harap Masukan 1 - 7";
 }
 
-int main()
-{
-    PannDev
-            cout
-        << solve() << endl;
-    r0
+int main () {
+	PannDev
+	cout << solve() << endl;
+	r0
 }
