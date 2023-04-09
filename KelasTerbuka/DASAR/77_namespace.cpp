b23fb35d6ecbc414c678da47f4ae68761edae7ce
PannDev <dryhezelnut@gmail.com>
2023-04-09 08:35:55 +0700

    namespace

---
+++ KelasTerbuka/DASAR/77_namespace.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#include "77_pann.h" // ambil dari luar
+
+#define PannDev std::ios::sync_with_stdio(0);
+#define nL <<std::endl
+
+/*
+77_pann.h
+*/
+
+void func() {
+    std::cout << "ini adalah fungsi biasa" nL nL;
+}
+
+using namespace pann;
+int main () {PannDev
+func(); // ini adalah fungsi biasa
+
+std::cout << pann::b nL;
+pann::pFunc();
+pann::cout(100);
+
+// using namespace pann;
+/*
+std::cout << b nL;
+pFunc();
+cout(100);
+*/
+
+std::cout nL;
+return 0;
+}
+
+/*
+namespace gunanya biar rapih
+*/
---
+++ KelasTerbuka/DASAR/77_pann.h
@@ -0,0 +1,12 @@
+namespace pann {
+    int b = 10;
+
+    void pFunc() {
+    std::cout << "ini adalah fungsi pann" << std::endl;
+    }
+
+    void cout(int a) {
+        std::cout << a << std::endl;
+    }
+
+}
