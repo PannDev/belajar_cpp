91cdbce149daf4221b5b146d9373d86fa07a6399
PannDev <86787046+PannDev@users.noreply.github.com>
2023-05-13 16:30:27 +0700

    prime

---
+++ studiKasus/bilPrima/solve.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define PannDev ios::sync_with_stdio(0);
+#define nL <<endl
+
+int main() { PannDev
+
+    int num; cout << "input number = "; cin >> num;
+    int mod = 0;
+
+    for (int i = num; i > 0; i--) {
+        int res = num % i;
+        if ( res == 0 ) {
+            mod++;
+        }
+    }
+    if (mod == 2) {
+        cout << num << " prima" nL;
+    } else {
+        cout << num << " not prima" nL;
+    }
+
+
+    return 0;
+}
