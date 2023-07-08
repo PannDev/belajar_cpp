43977ccde5b3130b4e0127d7efb671dbd0beaad1
PannDev <86787046+PannDev@users.noreply.github.com>
2023-07-08 11:46:58 +0700

    mau beresin(1)

---
+++ tlx/Pemograman Dasar/(07)_Perulangan_Lanjut/C_Pola_III.cpp
@@ -0,0 +1,55 @@
+//? Header Files and namespaces
+#include<bits/stdc++.h>
+using namespace std;
+
+//? Code Shortners
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define len(s) (int)s.size()
+#define coutn(x) cout<<x<<'\n'
+#define couts(x) cout<<x<<' '
+#define cout(x) cout<<x
+#define cin(x) cin>>x
+#define REP(i,a,b) for( int i = a; i <= b; i++)
+#define all(a) (a).begin(), (a).end()
+#define endl '\n'
+#define PannDev ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+ll mod = 1000000007;
+
+//? void
+void solve() {
+    
+    int n; cin(n);
+    int num = 0;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j <= i; j++) {
+            if (num == 10) {
+                num = 0;
+                cout(num);
+                num++;
+            } else {
+                cout(num);
+                num++;
+            }
+        }
+        cout(endl);
+    }
+    
+    return;
+}
+
+//! main
+int main () {
+    PannDev solve();
+
+    
+
+    return 0;
+}
