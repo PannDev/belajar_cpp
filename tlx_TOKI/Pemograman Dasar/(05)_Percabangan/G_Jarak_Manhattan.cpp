c7452aaf50a163f3a8f6f9460f04ce26f867372f
PannDev <86787046+PannDev@users.noreply.github.com>
2023-07-10 18:58:01 +0700

    done percabangan

---
+++ tlx_TOKI/Pemograman Dasar/(05)_Percabangan/G_Jarak_Manhattan.cpp
@@ -0,0 +1,37 @@
+//? Header Files and namespaces
+#include<bits/stdc++.h>
+using namespace std;
+
+//? Code Shortners
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define POP pop_back
+#define len(s) (int)s.size()
+#define coutn(x) cout<<x<<'\n'
+#define couts(x) cout<<x<<' '
+#define cout(x) cout<<x
+#define cin(x) cin>>x
+#define REP(i,a,b) for( int i = a; i <= b; i++)
+#define all(a) (a).begin(), (a).end()
+#define endl '\n'
+#define PannDev ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define slv solve();
+
+ll mod = 1000000007;
+
+//? void
+void solve() {
+    
+    int x1, x2, y1, y2; cin(x1>>y1>>x2>>y2);
+    coutn(abs(x1-x2)+abs(y1-y2));
+    
+}
+
+//! main
+int main () { PannDev slv return 0; }
