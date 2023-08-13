a78c9029a350348a262cd843bc36e3e0be3cc085
PannDev <86787046+PannDev@users.noreply.github.com>
2023-08-13 17:19:49 +0700

    compfest prob

---
+++ CPC_COMPFEST_15/Pemograman_Dasar/C_Hari_Penimbangan.cpp
@@ -0,0 +1,33 @@
+//? Header Files and namespaces
+#include<bits/stdc++.h>
+using namespace std;
+
+//? Code Shortners
+typedef long long ll;
+typedef unsigned long long ull;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define POP pop_back
+#define len(s) (int)s.size()
+#define all(a) (a).begin(), (a).end()
+#define endl '\n'
+#define PannDev ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define slv solve();
+
+const ll mod = pow(10, 9) + 7;
+
+//? void
+void solve() {
+    
+    double a,b,c,d,e,f; cin >> a >> b >> c >> d >> e >>f;
+    double res = (a + b + c + d + e + f) / 6;
+    cout << fixed << setprecision(9) << res << endl;
+    
+}
+
+//! main
+int main () { PannDev slv return 0; }
